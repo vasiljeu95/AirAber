e4df3ba61c357741501ad2e75162534fbbe082e2
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-02-16 17:55:12 +0300

    registrationButton.1

---
+++ Watch Extension/Controllers/CheckInInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  CheckInInterfaceController.swift
+//  Watch Extension
+//
+//  Created by Stepan Vasiljeu on 2/16/19.
+//  Copyright © 2019 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class CheckInInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
